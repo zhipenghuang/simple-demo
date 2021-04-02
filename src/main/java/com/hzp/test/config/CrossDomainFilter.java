ed862ca5c533c3bcb0a989e76aef7fc032b0cd46
云木 <yunmu@longops.com>
2021-04-02 14:57:02 +0700

    切面

---
+++ src/main/java/com/hzp/test/config/CrossDomainFilter.java
@@ -0,0 +1,45 @@
+package com.hzp.test.config;
+
+import cn.hutool.core.util.StrUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@WebFilter(filterName = "crossDomainFilter", urlPatterns = "/*")
+@Order(0)
+public class CrossDomainFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        //跨域处理
+        response.addHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        response.addHeader("Access-Control-Allow-Credentials", "true");
+
+        if (request.getHeader("Access-Control-Request-Method") != null && "OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.addHeader("Access-Control-Allow-Methods", "GET, POST");
+            response.addHeader("Access-Control-Allow-Headers", "X-Requested-With, Origin, Content-Type, Cookie,Authorization,tk");
+        }
+
+        //封装可重复读body的request
+        String POST = "post";
+        String JSON = "application/json";
+        if (POST.equalsIgnoreCase(request.getMethod()) && StrUtil.indexOfIgnoreCase(request.getContentType(), JSON) > -1) {
+            filterChain.doFilter(new CustomRequestWrapper(request), response);
+        } else {
+            filterChain.doFilter(request, response);
+        }
+    }
+}
